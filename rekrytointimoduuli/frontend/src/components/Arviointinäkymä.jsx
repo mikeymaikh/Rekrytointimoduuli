16a34de7512b503f728f959ed81f3f59b2e4b566
Mikael Gourine <mikael.gourine@edu.karelia.fi>
2025-03-17 14:23:17 +0200

    Add application components: Hakemuslista, HakijanTiedot, and Arviointinäkymä

---
+++ rekrytointimoduuli/frontend/src/components/Arviointinäkymä.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Arviointinäkymä = () => {
+  return (
+    <div>
+      <h2>Arviointinäkymä</h2>
+      {/* Add evaluation view content here */}
+    </div>
+  );
+};
+
+export default Arviointinäkymä;
---
+++ rekrytointimoduuli/frontend/src/components/HakijanTiedot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HakijanTiedot = () => {
+  return (
+    <div>
+      <h2>Hakijan Tiedot</h2>
+      {/* Add applicant details here */}
+    </div>
+  );
+};
+
+export default HakijanTiedot;
