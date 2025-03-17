456d020f47db648150779d11803a087d465a3971
Mikael Gourine <mikael.gourine@edu.karelia.fi>
2025-03-17 14:23:17 +0200

    Add application components: Hakemuslista, HakijanTiedot, and Arviointinäkymä

---
+++ rekrytointimoduuli/frontend/src/components/Hakemuslista.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hakemuslista = () => {
+  return (
+    <div>
+      <h2>Hakemuslista</h2>
+      {/* Add your list of applications here */}
+    </div>
+  );
+};
+
+export default Hakemuslista;
